8eee098e323a130a2bc8801da4b9f7da949a43ed
Sidra Paracha <sidraparacha27@gmail.com>
2022-10-28 03:49:22 -0700

    Bottom Navigation

--- app/src/main/java/com/sidrakotlin/navigationcomponent/MainActivity.kt
+++ app/src/main/java/com/sidrakotlin/navigationcomponent/MainActivity.kt
@@ -2,10 +2,24 @@ package com.sidrakotlin.navigationcomponent
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.NavHostController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var nav:BottomNavigationView
+    private lateinit var navController:NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nav = findViewById(R.id.bottomNav)
+
+        val navHost = supportFragmentManager.findFragmentById(R.id.fragmentContainerView) as NavHostFragment
+        navController = navHost.navController
+        setupWithNavController(nav, navController)
     }
 }
